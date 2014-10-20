be178b01ea6ff03b6a7ccb8c042d5e29ea494511
merilinm <merilin.metsamaa@khk.ee>
2014-10-20 13:48:20 +0300

    Add sample variables

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>harjutus-3 - Andmetüübid</title>
+</head>
+
+<body>
+    <h2>Muutujad</h2>
+    <?php
+    $variable = 90;
+    echo $variable;
+    // Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+    echo "<br>";
+    $variable = 40;
+    echo $variable;
+?>
+</body>
+</html>
